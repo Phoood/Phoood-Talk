bb5c1fcd29c033c4e6e15d3b10010c0112d505db
Chris <chriscabreros27@gmail.com>
2016-07-14 22:17:23 -0700

    you can send pics to each other with front facing camera

--- app/src/main/java/com/phooodstudio/phooodtalk/presentation/JournalFragment.java
+++ app/src/main/java/com/phooodstudio/phooodtalk/presentation/JournalFragment.java
@@ -1,5 +1,6 @@
 package com.phooodstudio.phooodtalk.presentation;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,6 +9,7 @@ import android.net.Uri;
 import android.os.Debug;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -19,16 +21,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.phooodstudio.phooodtalk.R;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Created by Christopher Cabreros on 27-Jun-16.
@@ -43,15 +55,21 @@ public class JournalFragment extends Fragment {
     private ImageView mImageView;
     private Button mButton;
     private File mImageFile;
-    private DatabaseReference myRef;
+    private File mDownloadFile;
+    private StorageReference mStorageReference;
+    private DatabaseReference mDatabaseReference;
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_journal, container, false);
+        mContext = getActivity();
 
+        final FirebaseStorage storage = FirebaseStorage.getInstance();
+        mStorageReference = storage.getReferenceFromUrl("gs://phooodtalk.appspot.com ");
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("sharedPicture");
+        mDatabaseReference = database.getReference("picture");
 
         //Create directory
         try {
@@ -60,7 +78,8 @@ public class JournalFragment extends Fragment {
                     File.separator + "myDir" + File.separator);
             //noinspection ResultOfMethodCallIgnored
             root.mkdirs();
-            mImageFile = new File(root, "picture");
+            mImageFile = new File(root, "sharedPicture");
+            mDownloadFile = new File(root, "downloadPicture");
         } catch (Exception e) {
             System.err.println("Unable to create file for storage");
             e.printStackTrace();
@@ -80,12 +99,23 @@ public class JournalFragment extends Fragment {
             }
         });
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                Bitmap bitmap = stringToBitmap(value);
-                mImageView.setImageBitmap(bitmap);
+                String filename = dataSnapshot.getValue(String.class);
+                StorageReference downloadReference = mStorageReference.child(filename);
+                final Uri downloadURI = Uri.fromFile(mDownloadFile);
+                downloadReference.getFile(mDownloadFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        try {
+                            Bitmap loadBitmap  = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), downloadURI);
+                            mImageView.setImageBitmap(loadBitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
 
             @Override
@@ -103,17 +133,25 @@ public class JournalFragment extends Fragment {
         //Get the picture, to database
         if (requestCode == REQUEST_TAKE_PHOTO) {
             Log.d(TAG, "request code: request take photo");
-            Uri receivedUri = Uri.fromFile(mImageFile);
+
+            //Send picture to the storage
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), receivedUri);
-                Bitmap rotatedBitmap = rotateBitmap(bitmap, 270);
-                String bitmapString = bitmapToString(rotatedBitmap);
-                myRef.setValue(bitmapString);
-
-                bitmap.recycle();
-                rotatedBitmap.recycle();
-
-            } catch (IOException e) {
+                InputStream stream = new FileInputStream(mImageFile);
+                UploadTask uploadTask = mStorageReference.putStream(stream);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "unuccessful upload");
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //Send notification to database that there was success
+                        mDatabaseReference.setValue(new Date().getTime());
+                        Log.d(TAG, "successful upload");
+                    }
+                });
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
